219aa9f12814904295e5182cbcea755a17bc54fa
Mysmelf <matthewr9937@gmail.com>
2023-08-22 09:34:07 -0700

    Created working initial prompt that console logs input data

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// 
+var CharLength = 0;
 
 // Write password to the #password input
 function writePassword() {
@@ -9,6 +11,18 @@ function writePassword() {
   passwordText.value = password;
 
 }
+// 
+
+function generatePassword(){
+ charLength = prompt("Please enter a number for your desired password length between 8 and 126 characters", "8-126");
+ 
+ if (charLength < 8) {
+  console.log('Sorry, your password is too short!');
+} else {
+  console.log(charLength);
+}
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+// 
