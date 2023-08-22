edf6e5bad6dc40f2dc6244fa0be9fa430ca72068
Mysmelf <matthewr9937@gmail.com>
2023-08-22 09:40:00 -0700

    Added minimum and maximum character limits to password generation prompt 1

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-// 
-var CharLength = 0;
+// ----------
+let CharLength = 0;
 
 // Write password to the #password input
 function writePassword() {
@@ -11,18 +11,23 @@ function writePassword() {
   passwordText.value = password;
 
 }
-// 
+// ---------
+
+// Password Generating Code Block
 
 function generatePassword(){
  charLength = prompt("Please enter a number for your desired password length between 8 and 126 characters", "8-126");
  
  if (charLength < 8) {
-  console.log('Sorry, your password is too short!');
+  alert('Your password is too short! Please type a number between 8 and 126');
+} else if (charLength > 126) {
+  alert('Your password is too large! Please type a number between 8 and 126');
 } else {
   console.log(charLength);
 }
 }
+// -------
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-// 
+// ---------
